96eb5435654e75f09ae5f7eebff5c195461a0592
Umid <v.umajon@gmail.com>
2024-11-02 12:15:54 +0500

    new

---
+++ shapes.js
@@ -0,0 +1,52 @@
+export const shapes = {
+    S: {
+        shape: [[0, 1, 1],
+        [1, 1, 0]],
+        color: 'yellowgreen'
+    },
+
+    Z: {
+        shape: [[1, 1, 0],
+        [0, 1, 1]],
+        color: 'red'
+    },
+
+    T: {
+        shape: [[1, 1, 1],
+        [0, 1, 0]],
+        color: 'purple'
+    },
+
+    O: {
+        shape: [[1, 1],
+        [1, 1]],
+        color: 'yellow'
+    },
+
+    J: {
+        shape: [[0, 1],
+        [0, 1],
+        [1, 1]],
+        color: 'blue'
+    },
+
+    L: {
+        shape: [[1, 0],
+        [1, 0],
+        [1, 1]],
+        color: 'orange'
+    },
+
+    I: {
+        shape: [[1],
+        [1],
+        [1],
+        [1]],
+        color: 'aqua'
+    }
+}
+
+export function fn(){
+    console.log('fn')
+}
+
